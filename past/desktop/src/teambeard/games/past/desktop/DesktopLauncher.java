dbf5c474eec697952d6e6bf05243107eea0db4a4
Hallgeir Løkken <istarnion@gmail.com>
2014-11-18 08:56:10 +0100

    Project setup

---
+++ past/desktop/src/teambeard/games/past/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package teambeard.games.past.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import teambeard.games.past.PAST;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PAST(), config);
+	}
+}
